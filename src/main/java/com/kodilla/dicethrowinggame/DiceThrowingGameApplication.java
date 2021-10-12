944bb81d27563211b8c78a517ab65db3d29a0779
Katarzyna Pikula <k.pikula@wp.eu>
2021-10-12 10:09:05 +0200

    Initial set-up

---
+++ src/main/java/com/kodilla/dicethrowinggame/DiceThrowingGameApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.dicethrowinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceThrowingGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiceThrowingGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/dicethrowinggame/DiceThrowingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.dicethrowinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceThrowingGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
